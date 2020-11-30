a47f3d2c63ce59a1db442b39d949900375660062
mackdunkan <mackdunkan@gmail.com>
2020-11-30 23:20:33 +0200

    Update Footer.elm

--- src/Components/Footer.elm
+++ src/Components/Footer.elm
@@ -1,7 +1,7 @@
 module Components.Footer exposing (..)
 
-import Css exposing (backgroundColor, color)
-import Html.Styled exposing (Html, a, div, footer, p, text)
+import Css exposing (Color, backgroundColor, color, int, vw, width, zIndex)
+import Html.Styled exposing (Attribute, Html, a, div, footer, p, styled, text)
 import Html.Styled.Attributes exposing (css, href)
 import Spa.Generated.Route exposing (Route)
 import TW.Breakpoints exposing (atBreakpoint, lg, md, sm, xl)
@@ -25,12 +25,14 @@ view options =
                     , ( md, TW.flex )
                     , ( md, TW.flex_row )
                     , ( md, TW.flex_row_reverse )
+                    , ( md, TW.items_end )
                     ]
                 ]
             ]
             [ infoBlock
-            , div [ css [ atBreakpoint [ ( md, TW.w_1over2 ), ( md, TW.space_y_8 ) ], padding, TW.space_y_11 ] ]
-                [ logoBlock
+            , div [ css [ TW.relative, TW.z_10, TW.bg_white, atBreakpoint [ ( md, TW.w_1over2 ), ( md, TW.space_y_8 ) ], padding, TW.space_y_11 ] ]
+                [ bgBlockSub [ css [ TW.bg_white, TW.right_0 ] ] []
+                , logoBlock
                 , menuBlock
                 ]
             ]
@@ -43,6 +45,17 @@ padding =
         [ atBreakpoint [ ( md, TW.py_16 ), ( lg, TW.px_6 ), ( lg, TW.py_24 ), ( xl, TW.px_8 ) ]
         , TW.py_11
         , TW.px_4
+        ]
+
+
+bgBlockSub : List (Attribute msg) -> List (Html msg) -> Html msg
+bgBlockSub =
+    styled div
+        [ TW.absolute
+        , TW.inset_y_0
+        , TW.h_full
+        , width <| vw 50
+        , zIndex <| int -1
         ]
 
 
@@ -59,12 +72,22 @@ infoBlock =
     in
     div
         [ css
-            [ atBreakpoint [ ( md, TW.w_1over2 ), ( md, TW.pl_20 ), ( lg, TW.pl_24 ), ( xl, TW.pl_24 ) ]
+            [ atBreakpoint
+                [ ( md, TW.w_1over2 )
+                , ( md, TW.pl_20 )
+                , ( md, TW.pb_60 )
+                , ( lg, TW.pl_24 )
+                , ( lg, TW.pb_40 )
+                , ( xl, TW.pl_24 )
+                , ( xl, TW.pb_36 )
+                ]
+            , TW.relative
             , backgroundColor TM.green
             , padding
             ]
         ]
-        [ div [ css [ atBreakpoint [ ( md, TW.gap_6 ), ( lg, TW.gap_12 ) ], TW.grid, TW.gap_4 ] ]
+        [ bgBlockSub [ css [ backgroundColor TM.green, TW.left_0 ] ] []
+        , div [ css [ TW.relative, TW.z_10, atBreakpoint [ ( md, TW.gap_6 ), ( lg, TW.gap_12 ) ], TW.grid, TW.gap_4 ] ]
             [ infoItem "Phone number" <| a [ href <| "tel:" ++ phone ] [ text phone ]
             , infoItem "E-mail" <| a [ href <| "mailto:" ++ email ] [ text email ]
             , infoItem "Address" <| p [] [ text "53/66 Baghramyan street, Yerevan, Armenia" ]
